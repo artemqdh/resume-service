b2c0084068b1e1a7d3c2bce864c5e1cbbca1984e
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2026-04-19 20:36:56 +0300

    слив веток (#5)
    * избавился от тегов при получении rawDescription

--- Domain/Entity/Vacancy.cs
+++ Domain/Entity/Vacancy.cs
@@ -5,11 +5,5 @@
         public int Id { get; set; }
         public required string Title { get; set; }
         public required string Description { get; set; }
-        public string? NameCompany { get; set; }
-        public string? DatePosted { get; set; }
-        public string? Country { get; set; }
-        public string? StreetAddress { get; set; }
-        public string? WorkSchedule { get; set; }
-       // public string? WorkSchedule { get; set; }
     }
 }
